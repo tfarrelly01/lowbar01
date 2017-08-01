9487627def5817161716a65c5aa14055276a4cc7
Tony Farrelly <tfarrelly01@gmail.com>
2017-08-01 09:55:10 +0100

    [lowbar01][_.identity] accepts one parameter, returns undefined if no argument passed to the function

--- spec/lowbar01.spec.js
+++ spec/lowbar01.spec.js
@@ -3,11 +3,24 @@ const path = require('path');
 const expect = require('chai').expect;
 const _ = require(path.join(__dirname, '..', './lowbar01.js'));
 
-describe('_', () => {
+describe('_', function () {
   'use strict';
 
-  it('is an object', () => {
+  it('is an object', function () {
     expect(_).to.be.an('object');
   });
-  
+
+  describe('#identity', function () {
+    it('is a function', function () {
+      expect(_.identity).to.be.a('function');
+    });
+    it('should take 1 argument', function () {
+      expect(_.identity.length).to.equal(1);
+    });
+    it('returns undefined if called with no arguments', function () {
+      var result = _.identity();
+      expect(result).to.equal(undefined);
+    });
+  });
+
 }); 
