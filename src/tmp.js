285ab0bf8394c581b8176ae66fe23a79a4a45e03
Tony Farrelly <tfarrelly01@gmail.com>
2017-09-04 11:49:51 +0100

    [lowbar01] remove tmp.js file previously committed in error

--- src/tmp.js
+++
@@ -1,5 +0,0 @@
-function checkIfExistingValue (obj, key, value) {
-    return obj.hasOwnProperty(key) && obj[key] === value;
-}
-var test = [{name : 'jack', sex: 'F'}, {name: 'joe', sex: 'M'}];
-console.log(test.some(function (person) { return checkIfExistingValue(person, 'name', 'jack'); })); 
